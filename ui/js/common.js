846f418635015fb847695b3b903989d471ab235e
sosim13 <sosim13p@gmail.com>
2018-03-28 16:59:45 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -40,3 +40,22 @@ css("#listWrap.index h2 span", "background-color", "rgba(255, 255, 255, 0.01)");
 window.setTimeout("agate.runScript('caller.showNow')", 500);
 
   
+css("body", "background-color", "rgba(255, 255, 255, 0.01)");
+css("body", "zoom", "percentage");
+//goZoom();
+css("#listWrap.index h2 span", "background-color", "rgba(255, 255, 255, 0.01)");
+css("body", "font-size", "24px");
+css("a:link", "font-size", "24px");
+css("a:visited", "font-size", "24px");
+css("a:hover", "font-size", "24px");
+css("tr, td, th", "font-size", "24px");
+css("tr, td, th", "font-size", "24px");
+css("table.bbslist td#tdate", "font-size", "1px");
+
+css("table.bbslist", "width", "1000px");
+css("#t_right", "margin", "0 0 0 0");
+css("div#banner_box", "width", "1000px");
+css("div#banner_box", "margin", "0 0 0 0");
+css("table.bbslist td#tcat", "white-space", "nowrap");
+css("table.bbslist td#tsub", "width", "700px");
+css("table.bbslist td#tsub", "table-:layout", "pixed");
