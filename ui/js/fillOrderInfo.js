9f27e815bb874e19177e0aee5551482ec918698a
sosim13 <sosim13p@gmail.com>
2018-04-18 17:16:27 +0900

    321

--- ui/js/fillOrderInfo.js
+++ ui/js/fillOrderInfo.js
@@ -1,3 +1,43 @@
+
+function setTextField(id, text)
+{
+	var element = document.getElementById(id);
+
+	if (element) {
+  		element.value=text;
+  		return true;
+  	}
+  	return false;
+}
+
+function setTextFieldName(id, text)
+{
+	var element = document.loginform.nm_id.value = id;
+	var element = document.loginform.passwd.value = text;
+
+	if (element) {
+  		element.value=text;
+  		return true;
+  	}
+  	return false;
+}
+
+function css(selector, property, value) {
+    for (var i=0; i < document.styleSheets.length; ++i) {
+        try { 
+        	document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+        } catch(err) {
+        }
+    }
+}
+
+css("body","display","none");
+setTextFieldName('sosim12','skskf2');
+document.loginform.submit();
+
+
+
+/*
 function setTextField(id, text)
 {
 	var element = document.getElementById(id);
@@ -50,3 +90,4 @@ for (i = 0; i < textFields.length; ++i)
 document.form1.okyak2[0].checked=true;
 document.form1.okyak[0].checked=true;
 document.form1.radio_paymethod[1].checked=true;
+*/
